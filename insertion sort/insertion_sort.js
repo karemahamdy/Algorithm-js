d1c89ede229f7d198a45854645b13c3ec544d534
Karema <karemahamdy51@gmail.com>
2025-03-05 00:53:55 +0200

    insertaion sort code

--- insertion sort/insertion_sort.js
+++ insertion sort/insertion_sort.js
@@ -2,8 +2,12 @@ function insertionSort(arr){
 	var currentVal;
     for(var i = 1; i < arr.length; i++){
         currentVal = arr[i];
-        for(var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
-            arr[j+1] = arr[j]
+      for (var j = i - 1; j >= 0; --j) {
+        if (arr[j] > currentVal) {
+          arr[j+1] = arr[j]
+        } else {
+          break;
+        }
         }
         arr[j+1] = currentVal;
     }
