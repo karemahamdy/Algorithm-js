70205db30ad313524c7cadc06c7d22f2caba23b8
Karema <karemahamdy51@gmail.com>
2025-03-07 02:49:11 +0200

    feat add recursion

--- recursion/recursion.js
+++ recursion/recursion.js
@@ -1,1 +1,27 @@
 // Recursion is a programming technique where a function calls itself repeatedly to solve a problem
+
+// Program to countdown till 1
+
+// recursive function
+function counter(count) {
+
+  // display count
+  console.log(count);
+
+  // condition for stopping
+  if (count > 1) {
+
+    // decrease count
+    count = count - 1;
+
+    // call counter with new value of count
+    counter(count);
+  } else {
+
+    // terminate execution
+    return;
+  };
+};
+
+// access function
+counter(5);
