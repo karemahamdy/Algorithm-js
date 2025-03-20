f0b4bffac186d9c18e048b933a42e78342d5ef41
Karema <karemahamdy51@gmail.com>
2025-03-21 00:49:12 +0200

    feat: 704. Binary Search

---
+++ binary search/implementation.js
@@ -0,0 +1,24 @@
+// 704. Binary Search
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (arr, elem) {
+  let start = 0
+  let end = arr.length - 1
+  let middle = Math.floor((start + end) / 2)
+  while (arr[middle] !== elem && start <= end) {
+    if (elem < arr[middle]) {
+      end = middle - 1;
+    } else {
+      start = middle + 1;
+    }
+    middle = Math.floor((start + end) / 2);
+  }
+  if (arr[middle] === elem) {
+    return middle;
+  }
+  return -1;
+}
